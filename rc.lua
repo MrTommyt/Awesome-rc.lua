4feef244a619acc19b42b68e4c45a819ebfa811a
MrTommyt <benavidescamilo199@gmail.com>
2020-07-19 10:11:08 -0500

    borde de los clientes redondeados

--- rc.lua
+++ rc.lua
@@ -562,7 +562,10 @@ client.connect_signal("manage", function (c)
     -- Set the windows at the slave,
     -- i.e. put it at the end of others instead of setting it master.
     -- if not awesome.startup then awful.client.setslave(c) end
-
+		c.shape = function(cr,w,h)
+        gears.shape.rounded_rect(cr, w, h, 10)
+    end
+		
     if awesome.startup and
       not c.size_hints.user_position
       and not c.size_hints.program_position then
